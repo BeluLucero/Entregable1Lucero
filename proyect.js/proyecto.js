eb6090b740469001118313535fde40c1aa410187
belulucero <belenlucero3177@gmail.com>
2024-08-02 20:16:50 -0300

    cambios

---
+++ proyect.js/proyecto.js
@@ -0,0 +1,34 @@
+const nombre = prompt ("Diga su nombre")
+console.log(nombre)
+alert ("Bienvenidos a Herbolity")
+
+let agregar = 0
+console.log(agregar)
+
+let producto = 0
+console.log(producto)
+alert ("carrito vacio")
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Colágeno en polvo",
+        precio: 30000.00,
+        descripcion: "Polvo de colégeno hidrolizado",
+        categoria: "Suplementos"
+    },
+    {
+        id: 2,
+        nombre: "Probióticos",
+        precio: 25000.00,
+        descripcion: "Comprimidos en cápsulas blandas para el buen funcionamiento del sistema digestivo",
+        categoria: "Suplementos"
+    },
+    {
+        id: 3,
+        nombre: "Jugo natural de aloe vera",
+        precio: 9000.00,
+        descripcion: "Depurativo del sistema digestivo",
+        categoria: "Suplementos"
+    }
+];
